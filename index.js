6c36105d50c8e195d5f36a1d5ead6314c0fb0d91
maksudalam <maksud.cdz@gmail.com>
2022-11-25 22:42:08 +0600

    buyer order complete

--- index.js
+++ index.js
@@ -20,19 +20,40 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 async function run(){
     try{
         const bikeCategoriesCollection=client.db('bikeResell').collection('bikeCategories')
+        const buyerCollection=client.db('bikeResell').collection('buyer')
+        const usersCollection=client.db('bikeResell').collection('users')
+      
 
         app.get('/bikeCategories',async(req,res)=>{
             const query={};
             const Categories=await bikeCategoriesCollection.find(query).toArray();
             res.send(Categories)
         })
+       
         app.get('/bikeCategories/:id',async(req,res)=>{
             const id=req.params.id
             console.log(id)
-            const query={_id:ObjectID(id)};
+          const query={_id:ObjectID(id)}
+          console.log(query)
             const category=await bikeCategoriesCollection.findOne(query)
             res.send(category)
         })
+        app.post('/buyer',async(req,res)=>{
+            const buyer=req.body
+            const result=await buyerCollection.insertOne(buyer)
+            res.send(result)
+        })
+        app.get('/buyer',async(req,res)=>{
+            const email=req.query.email
+            const query={email:email}
+            const buyer=await buyerCollection.find(query).toArray()
+            res.send(buyer) 
+        })
+        app.post('/users',async(req,res)=>{
+            const users=req.body
+            const result=await usersCollection.insertOne(users)
+            res.send(result)
+    })
 
 
     }
