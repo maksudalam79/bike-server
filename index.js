69ada5a3ff217df3b5095bd73b0ef07e32d59f3f
maksudalam <maksud.cdz@gmail.com>
2022-11-25 23:28:18 +0600

    jwt token

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const { MongoClient, ServerApiVersion } = require('mongodb');
+const jsonwebtoken = require('jsonwebtoken');
 const { ObjectID } = require('bson');
 const port=process.env.PORT||5000;
 require('dotenv').config()
@@ -49,6 +50,18 @@ async function run(){
             const buyer=await buyerCollection.find(query).toArray()
             res.send(buyer) 
         })
+app.get('/jwt',async(req,res)=>{
+    const email=req.query.email
+    const query={email:email}
+    const user=await usersCollection.findOne(query)
+    if(user){
+        const token=jwt.sign({email},process.env.ACCESS_TOKEN)
+        return res.send({accessToken:token})
+    }
+    res.status(403).send({accessToken:''})
+})
+
+
         app.post('/users',async(req,res)=>{
             const user=req.body
             const result=await usersCollection.insertOne(user)
